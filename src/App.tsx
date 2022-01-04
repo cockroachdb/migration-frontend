f4d7cbeb72cdc63855afb71e046e4822e8ee6927
Oliver Tan <otan@cockroachlabs.com>
2022-01-04 12:28:07 +1100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,128 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+
+import axios from 'axios';
+
+interface Import {
+  id: string;
+  unix_nano: number;
+  import_metadata: ImportMetadata;
+}
+
+interface ImportMetadata {
+  statements: ImportStatement[];
+}
+
+interface ImportStatement {
+  original: string;
+  cockroach: string;
+  issues: ImportIssue[];
+}
+
+interface ImportIssue {
+  type: string;
+  text: string;
+}
+
+class App extends React.Component<{}, Import> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      id: '',
+      unix_nano: 0,
+      import_metadata: {statements: []},
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+  }
+
+  componentDidMount() {
+    this.reloadPage()
+  }
+
+  reloadPage() {
+    axios.get<Import>("http://localhost:5050/get").then(
+      response => {
+        this.setState({...response.data});
+      }
+    ).catch(
+      error => console.error(`Error: ${error}`)
+    );
+  }
+
+  handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    console.log(this.state.import_metadata.statements[0].cockroach);
+  }
+
+  handleTextAreaChange(idx: number) {
+    return (event: React.ChangeEvent<HTMLTextAreaElement>) => this.handleTextAreaChangeForIdx(idx, event)
+  }
+
+  handleTextAreaChangeForIdx  ( idx: number, event: React.ChangeEvent<HTMLTextAreaElement>) {
+    const newState = this.state
+    newState.import_metadata.statements[idx].cockroach = event.target.value;
+    this.setState(newState);
+  }
+
+  render() {
+    return (
+      <Container className="p-3">
+        <Container className="bg-light text-dark p-5" fluid>
+          <Container className="bg-light p-5">
+            <h1 className="display-4 fw-bold">
+              {this.state.id == '' ? 'Loading database migration....' : this.state.id}
+            </h1>
+            <hr/>
+            <p>{new Date(this.state.unix_nano / 1000000).toISOString()}</p>
+          </Container>
+        </Container>
+
+        <form onSubmit={this.handleSubmit}>
+          <Button variant="primary" type="submit">Reimport</Button>
+          <Container className="p-4" fluid>
+              <Row className="m-2 p-2">
+                <Col xs={4}><strong>PostgreSQL statement</strong></Col>
+                <Col xs={4}><strong>CockroachDB statement</strong></Col>
+                <Col xs={4}><strong>Issues</strong></Col>
+              </Row>
+          {this.state.import_metadata.statements.length > 0 ? this.state.import_metadata.statements.map((statement, idx) => (
+            <Row className={"m-2 p-2 border " + (statement.issues != null && statement.issues.length > 0 ? 'border-danger': '')}>
+              <Col xs={4}>
+                <pre>{statement.original}</pre>
+              </Col>
+              <Col xs={4}>
+                    <textarea className="form-control" id={'ta' + idx} value={statement.cockroach} onChange={this.handleTextAreaChange(idx)}/>
+              </Col>
+              <Col xs={4}>
+                  <ul>
+                    {statement.issues != null && statement.issues.length > 0 ? statement.issues.map((issue, idx) => (
+                        <li key={'li' + idx}>{issue.text}</li>
+                    )): ''}
+                  </ul>
+              </Col>
+            </Row>
+          )) : (
+            <Row className="justify-content-md-center">
+                 <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                  </Spinner>
+            </Row>
+            )
+          }
+        </Container>
+        </form>
+
+        <hr/>
+        <p>The early 2010s called, they want their Twitter Bootstrap theme back!</p>
+      </Container>
+    );
+  }
 }
 
 export default App;
