505d8ba476b3b9580eff134ab787384e849b1eb0
Oliver Tan <otan@cockroachlabs.com>
2022-01-13 16:54:17 +1100

    add estimated fix times

--- src/App.tsx
+++ src/App.tsx
@@ -670,6 +670,9 @@ function StatementsSummary(props: {statements: ImportStatement[]}) {
   var numDanger = 0;
   var numInfo = 0;
 
+  const unimplementedIssueCount = new Map<string, number>();
+  const re = /issue-v\/([0-9]*)/i;
+
   props.statements.forEach((statement) => {
     numStatements++;
     if (statement.issues != null) {
@@ -680,15 +683,69 @@ function StatementsSummary(props: {statements: ImportStatement[]}) {
             break;
           default:
             numDanger++;
+
+            if (issue.type === "unimplemented") {
+              const match = issue.text.match(re);
+              if (match != null) {
+                const issue = match[1];
+                const curr = unimplementedIssueCount.get(issue);
+                unimplementedIssueCount.set(issue, curr != null ? curr + 1: 1);
+              }
+            }
         }
       })
     }
   })
 
+  const unimplementedIssueCountArr = Array.from(unimplementedIssueCount);
+  unimplementedIssueCountArr.sort();
+
+  const estimatedEffort: {[key: string]: { issue: string; estimate: number;}} = {
+    '17511': {
+      issue: 'stored functions/procedures',
+      estimate: 1000 * 60 * 60 * 24 * 7,
+    },
+    '31632': {
+      issue: 'deferrable FKs',
+      estimate: 1000 * 60 * 60 * 24 * 7,
+    },
+    '74777': {
+      issue: 'pg extensions',
+      estimate: 1000 * 60 * 60 * 24 * 7,
+    },
+    '74780': {
+      issue: 'grant privilege on sequences',
+      estimate: 1000 * 60,
+    }
+  }
+
+  var totalTime = 0;
+  var totalUncatFixes = numDanger;
+  const estimatedTimeList = (<ul>
+    {unimplementedIssueCountArr.map(([issue, count], idx) => {
+      const estimate = estimatedEffort[issue];
+      if (estimate != null) {
+        totalTime += estimate.estimate * count;
+      }
+      totalUncatFixes -= count;
+      return (<li key={idx}>
+        <a href={`https://go.crdb.dev/issue-v/${issue}/dev`} target="_blank" rel="noreferrer">Issue #{issue}</a> encountered {count} times
+        {estimate != null ? <>
+          . Feature '{estimate.issue}' would take approx&nbsp;<Moment date={new Date(Date.now() - estimate.estimate*count)} fromNow ago/>&nbsp;to migrate.
+        </>: ''}
+      </li>);
+    })}
+    <li>{totalUncatFixes} uncategorised items.</li>
+  </ul>)
+
   return (
     <ul>
       <li>{numStatements} statements found.</li>
-      <li style={numDanger > 0 ? {color: 'red'}: {}}>{numDanger} fixes required.</li>
+      <li style={numDanger > 0 ? {color: 'red'}: {}}>{numDanger} fixes required.
+        {totalTime > 0 ? <>
+          &nbsp;Estimated fix time: <Moment date={new Date(Date.now() - totalTime)} fromNow ago/>.
+        </>: ''}</li>
+      {estimatedTimeList}
       <li style={numInfo > 0 ? {color: 'blue'} : {}}>{numInfo} optional audits.</li>
     </ul>
   )
